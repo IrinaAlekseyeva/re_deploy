db04e9c9f9dd5769edf04570097282abbb779e6d
Irina Alekseyeva <irinaalekseyeva@MacBook-Pro.local>
2025-01-16 00:02:12 -0800

    i

--- backend/config/database.js
+++ backend/config/database.js
@@ -13,7 +13,7 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variable: 'postgresql://irina_db_q9lg_user:fuQaHF1c2aPAz3fjQCMX8Wm4YkoHWuB8@dpg-cu4a3ndds78s739qc7a0-a/irina_db_q9lg',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
